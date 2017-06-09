86eb5b4736c373e1355e7199d4de22ddffa9538e
Marina Gabriela Bezerra <mgb@ug5c20.windows.cin.ufpe.br>
2017-06-09 11:01:23 -0300

    Códigos incompletos e PDF do artigo.

---
+++ TreeDistance.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+
+# I(Pc), I(Pai), I(Pc ^ Pai)
+
+def I_Pc(dataset):
+    classes = np.array(dataset['classe'].unique()) # taking dataset classes
+    result = 0
+    for classe in classes:
+        Pj = float(dataset[dataset['classe'] == classe].shape[0])/dataset.shape[0]
+        if Pj != 0:
+            result += Pj*np.log2(Pj)
+    return -result
+
+def I_PAi(dataset, attribute):
+    attr_values = np.array(dataset[attribute].unique()) # taking possible attribute values
+    result = 0
+    for value in attr_values:
+        Pi = float(dataset[dataset[attribute] == value].shape[0])/dataset.shape[0]
+        if Pi != 0:
+            result += Pi*np.log2(Pi)    
+    return -result
+
+def I_Pc_inter_PAi(dataset, attribute):
+    classes = np.array(dataset['classe'].unique()) # taking dataset classes
+    result = 0
+    for classe in classes:
+        attr_values = np.array(dataset[attribute].unique()) # taking possible attribute values
+        df_classe = dataset[dataset['classe'] == classe]
+        Pj = float(df_classe.shape[0])/dataset.shape[0] 
+        for value in attr_values:
+            # P(Ai inter C) = P(Ai|C)*P(C)
+            Pij = (float(df_classe[df_classe[attribute] == value].shape[0])/df_classe.shape[0])*Pj
+            if Pij != 0:
+                result += Pij*np.log2(Pij)
+    return -result
+
+def treeDistance(dataset, attribute):
+    term1 = I_Pc(dataset)/I_Pc_inter_PAi(dataset, attribute)
+    term2 = I_PAi(dataset, attribute)/I_Pc_inter_PAi(dataset, attribute)
+    return 2 - term1 - term2
+
+### MAIN
+exemplo = pd.read_csv('db.csv',header=None)
+exemplo.columns = ['aspecto','temperatura','humidade','vento','classe']
+attributes = exemplo.columns
+attributes = attributes[:-1]
+distances = {}
+for attribute in attributes:
+    distances[attribute] = treeDistance(exemplo, attribute)
+print(distances)
+
+# import julia
+# j = julia.Julia()
+# j.include("vdm_om.jl")
---
+++ vdm_om.jl
@@ -0,0 +1,192 @@
+# TO EXECUTE:
+# cd("this file and BDs files directory path")
+# include("questao_1.jl")
+
+# Pkg.add("PyCall")
+using DataFrames
+using Gadfly
+using PyCall
+
+# df = dataframe of DB
+# class = class column number starting from 1
+# return = dataframe with class colunm as the last column of df
+function prettyDf(df, class)
+    df[:Class] = df[class]
+    delete!(df, class)
+    return df
+end
+
+# db = DB dataframe 
+# p = separation percentage for train dataframe
+function train_test(df, p)
+    rows, cols = size(df)
+    limA = Int(floor(rows*p))
+    train = df[1:limA,:]
+    test = df[limA+1:rows,:]
+    return train, test
+end
+
+# df = dataframe with grouped by class neighbors and count column
+# return = class of tested instance
+function classifier(df)
+    result = sort(df, cols = :Count)
+    rows, cols = size(result)
+    return result[rows, :Class]
+end
+
+# k = n neighbor(s)
+# a = distance array 
+# train = train dataframe
+# flag = true if Weighted kNN
+# return = DataFrame with neighbors classes
+function findNeighbors(k, a, train, flag)
+    indexs = sortperm(a)
+    trows, tcols = size(train)
+    neighbors = DataFrame(Class = typeof(train[1, tcols])[])
+    if(flag)
+        neighbors[:w] = 0.0
+        for i in 1:size(indexs[range(1, k)], 1)
+            weight = 1/(a[indexs[i]] * a[indexs[i]])
+            push!(neighbors, [train[indexs[i], tcols], weight] )
+        end
+        neighbors1 = by(neighbors, :Class, d -> DataFrame(Count=sum(d[:w])))
+    else
+        for i in 1:size(indexs[range(1, k)], 1)
+            push!(neighbors, [train[indexs[i], tcols]])
+        end
+        neighbors1 = by(neighbors, :Class, d -> DataFrame(Count=nrow(d)))
+    end    
+    return neighbors1
+end
+
+# train = train dataframe
+# return = dataframe with all instances quantity given the classes
+function createTable(train)
+    rows, cols = size(train)
+    attr_names = names(train)
+    df_class = groupby(train, :Class)
+    len = length(df_class)
+    table = DataFrame(Attr = String[], Class = String[], Nic = Int[])
+    for i in 1:(cols-1) #loop through attributes
+        for c in 1:len #loop through classes
+            df = df_class[c]
+            class_name = df[1, :Class]
+            df_attribute_class = by(df, i, d -> DataFrame(Count=nrow(d)))           
+            for row in eachrow(df_attribute_class)
+                row_name = string(attr_names[i], "_", row[1])
+                n = row[:Count]
+                push!(table, [row_name, class_name, n])
+            end
+        end            
+    end
+    df = by(table, 1, d -> DataFrame(Ni=sum(d[:Nic])))
+    table[:Prob] = 0.0
+    for i in 1:size(df, 1)
+        for row in eachrow(table)
+            if(df[i, :Attr] == row[:Attr])
+                row[:Prob] = (row[:Nic])/(df[i, :Ni])
+                if(row[:Prob] > 1)
+                    println("erro!")
+                end
+            end    
+        end
+    end
+    delete!(df, [:Attr, :Ni])
+    delete!(table, :Nic)
+    for row in eachrow(table)
+        row[:Attr] = string(row[:Attr], "_", row[:Class])
+    end
+    delete!(table, :Class)
+    return table
+end
+
+# a = train instance
+# b = test instance
+function OM(a, b)
+    result = 0
+    for i in 1:(cols(a)-1) #loop through attributes
+        if(a[1, i] != b[1, i])
+            result = result + 1
+    end
+    return result
+end
+
+# train = train dataframe
+# table = probabilities dataframe
+# a = train instance
+# b = test instance
+# return = VDM distance of two instances (a,b) 
+function VDM(train, table, a, b)
+    rows, cols = size(train)
+    class_names = levels(train[cols])
+    attr_names = names(train)
+    result = 0
+    for i in 1:(cols-1) #loop through attributes
+        s = 0.0
+        for c in 1:length(class_names) #loop through classes
+            ra = string(attr_names[i], "_", a[1, i], "_", class_names[c])
+            rb = string(attr_names[i], "_", b[1, i], "_", class_names[c])
+            Piac, Pibc = 0.0, 0.0 
+            flaga, flagb = true, true
+            for row in eachrow(table)
+                if(ra == row[:Attr] && flaga)
+                    Piac = row[:Prob]
+                    flaga = false
+                end
+                if(rb == row[:Attr] && flagb)
+                    Pibc = row[:Prob]
+                    flagb = false
+                end
+                if(!flaga && !flagb)
+                    break
+                end
+            end
+            #println("Piac: ", Piac, " Pibc: ", Pibc)
+            sub = Piac - Pibc 
+            s = s + (sub * sub)
+        end
+        result = result + s
+    end
+    result = sqrt(result)
+    return result
+end
+
+# k = neighbor(s)
+# train = train dataframe
+# test = test dataframe
+# flag = true if Weighted kNN
+# return = classes of the test instances
+function kNN(k, train, test, flag)
+    trows, tcols = size(test)
+    rows, columns = size(train)
+    classes = Array(String, trows)
+    table = createTable(train)
+    for j in 1:trows
+        distances = Array(Float32, rows)
+        for i in 1:rows
+            distances[i] = VDM(train, table, train[i,:], test[j,:])
+        end
+        neighbors = findNeighbors(k, distances, train, flag)
+        classes[j] = classifier(neighbors)
+    end
+    return classes
+end
+
+
+######################################
+#                Main                #
+######################################
+
+############ FIRST DB ############## 
+println("** INICIANDO JULIA **")
+#Take DB and save it on a DataFrame
+train = readtable("db_train.csv", ',', header = false)
+test = readtable("db_test.csv", ',', header = false)
+train = prettyDf(train, cols(train)) #Put "Class" column on DF
+test = prettyDf(train, cols(train)) #Put "Class" column on DF
+#Using kNN
+k = 10
+classes = @time(kNN(k[i], train, test, false))
+#Calculating accuracy of this kNN
+accuracy = mean(classes .== test[cols(test)])
+
