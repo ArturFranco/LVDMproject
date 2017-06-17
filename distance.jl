334d4a080f95c81a8c6a81fb4758b7350d0cc465
Vinicius Sanguinete <evssm@cin.ufpe.br>
2017-06-17 09:54:12 -0300

    add distancia em julia

---
+++ distance.jl
@@ -0,0 +1,76 @@
+using DataFrames
+
+function getColumnValues(dfTable, column)
+    return convert(Array,sort(levels(dfTable[column])));
+end
+
+function I_Pc(df)
+    classes = getColumnValues(df,ncol(df))
+    result = 0
+    rows = nrow(df);
+    cols = ncol(df);
+    for class in classes
+        Pj = nrow(df[df[cols].== class,:])/rows
+        #print(Pj)
+        if(Pj != 0)
+            result += Pj*log2(Pj)
+        end
+    end
+    return -result
+end
+
+function I_PAi(df,attribute)
+    values = getColumnValues(df,attribute)
+    result = 0
+    rows = nrow(df);
+    cols = ncol(df);
+    for value in values
+        PAi = nrow(df[df[attribute].== value,:])/rows
+        #print(Pj)
+        if(PAi != 0)
+            result += PAi*log2(PAi)
+        end
+    end
+    return -result
+
+end
+
+function I_Pc_inter_PAi(df, attribute)
+    classes = getColumnValues(df,ncol(df))
+
+    rows = nrow(df)
+    cols = ncol(df)
+    result = 0
+
+    for class in classes
+        values = getColumnValues(df,attribute)
+        dfClass = df[df[cols].== class,:]
+        classRows = nrow(dfClass)
+        Pj = classRows/rows
+        for value in values
+            Pij = (nrow(dfClass[dfClass[attribute].== value,:])/classRows)*Pj
+            if(Pij != 0)
+                result += Pij*log2(Pij)
+            end
+        end
+    end
+    return - result
+end
+
+function treeDistance(df,attribute)
+    term1 = I_Pc(df)/I_Pc_inter_PAi(df, attribute)
+    term2 = I_PAi(df, attribute)/I_Pc_inter_PAi(df, attribute)
+    return 2 - term1 - term2
+end
+df = readtable("db.csv", separator = ',', header = false);
+
+println(I_Pc(df))
+println(I_PAi(df,1))
+println(I_Pc_inter_PAi(df,1))
+println(treeDistance(df,1))
+
+distances = []
+for col in 1:(ncol(df)-1)
+    push!(distances, treeDistance(df,col))
+end
+print(distances)
